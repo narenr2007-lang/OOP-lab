6f4e6482eb817908721fc5fc2cc21b451b40ef81
narenr2007-lang <narenr2007@gmail.com>
2026-03-05 09:34:05 +0530

    Add files via upload

---
+++ Java/Conditional statements/ifex.java
@@ -0,0 +1,11 @@
+public class ifex {
+    public static void main(String[] args) {
+
+        int number = 10;
+
+        if (number > 5) {
+            System.out.println("Number is greater than 5");
+        }
+
+    }
+}
